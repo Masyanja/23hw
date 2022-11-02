ff298b2cde771b9f5767ece8ff36c01031c7c856
Masyanja <107952755+Masyanja@users.noreply.github.com>
2022-11-02 10:52:41 +0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, request, jsonify
+from flask_restx import ValidationError
+
+from builder import build_query, build_query_2
+from models import RequestParams
+import functions
+
+main_bp = Blueprint('main', __name__)
+
+
+@main_bp.route('/perform_query', methods=['POST'])
+def perform_query():
+
+    try:
+        params = RequestParams().load(request.json)
+    except ValidationError as error:
+        return error.messages, 400
+
+    result = build_query(
+        cmd=params['cmd1'],
+        param=params['value1'],
+    )
+    result = build_query_2(cmd=params['cmd2'],
+                           param=params['value2'], res_data=result)
+    return jsonify(result)
+
